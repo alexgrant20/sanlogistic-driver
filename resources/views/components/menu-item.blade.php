88b0fa0eef178ae8b75d3d7e40e36a429e7c7b06
Steven Alexander <alexgrant200@yahoo.com>
2022-05-19 23:36:55 +0700

    init

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,5 +1,5 @@
 <div>
-  <a class="card {{ $backgroundColor }}">
+  <a href="{{ $link }}" class="card {{ $backgroundColor }}">
     <div class="card-body">
       <div class="d-flex flex-column align-items-center justify-content-center">
         <i class="bi {{ $icon }} fs-1 mb-3"></i>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,49 +3,29 @@
 @section('container')
   <div class="container-fluid">
     <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-xxl-5 g-4">
-      {{-- TO-DO : SIMPLIFY --}}
-      <?php if(empty($_SESSION['activity_id'])){ ?>
-      <div class="">
-        <a href="" class="card bg-blue">
-          <div class="card-body">
-            <div class="d-flex flex-column align-items-center justify-content-center">
-              <i class="bi bi-clipboard-plus-fill fs-1 mb-3"></i>
-              <span class="fw-bold fs-5 text-center">Activity</span>
-            </div>
-          </div>
-        </a>
-      </div>
-      <?php } else{ ?>
-      <div class="">
-        <a href="activity.php?source=update" class="card bg-green">
-          <div class="card-body">
-            <div class="d-flex flex-column align-items-center justify-content-center">
-              <i class="bi bi-clipboard-plus-fill fs-1 mb-3"></i>
-              <span class="fw-bold fs-5 text-center">Activity</span>
-            </div>
-          </div>
-        </a>
-      </div>
-      <?php } ?>
-      <div class="">
-        <a class="card bg-brown">
-          <div class="card-body">
-            <div class="d-flex flex-column align-items-center justify-content-center">
-              <i class="bi bi-clipboard2-check-fill fs-1 mb-3"></i>
-              <span class="fw-bold fs-5 text-center">Checklist</span>
-            </div>
-          </div>
-        </a>
-      </div>
+      <x-menu-item>
+        <x-slot name="backgroundColor">{{ empty($_SESSION['activity_id']) ? 'bg-blue' : 'bg-green' }}</x-slot>
+        <x-slot name="icon">{{ empty($_SESSION['activity_id']) ? 'bi-clipboard-plus-fill' : 'bi-flag' }}</x-slot>
+        <x-slot name="text">Activity</x-slot>
+        <x-slot name="link">{{ empty($_SESSION['activity_id']) ? '#' : '#' }}</x-slot>
+      </x-menu-item>
+      <x-menu-item>
+        <x-slot name="backgroundColor">bg-brown</x-slot>
+        <x-slot name="icon">bi-clipboard2-check-fill</x-slot>
+        <x-slot name="text">Checklist</x-slot>
+        <x-slot name="link">#</x-slot>
+      </x-menu-item>
       <x-menu-item>
         <x-slot name="backgroundColor">bg-purplish</x-slot>
         <x-slot name="icon">bi-arrow-clockwise</x-slot>
         <x-slot name="text">Perjalanan</x-slot>
+        <x-slot name="link">#</x-slot>
       </x-menu-item>
       <x-menu-item>
         <x-slot name="backgroundColor">bg-darkGreen</x-slot>
         <x-slot name="icon">bi-cash-coin</x-slot>
         <x-slot name="text">Keuangan</x-slot>
+        <x-slot name="link">#</x-slot>
       </x-menu-item>
     </div>
   </div>
